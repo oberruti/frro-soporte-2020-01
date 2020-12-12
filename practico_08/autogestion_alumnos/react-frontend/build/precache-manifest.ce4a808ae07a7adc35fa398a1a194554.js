2ef92d73cc77c6c5a517c4f7116e3919f3022f4f
oberruti <54275024+oberruti@users.noreply.github.com>
2020-12-12 00:56:52 -0300

    build

---
+++ practico_08/autogestion_alumnos/react-frontend/build/precache-manifest.ce4a808ae07a7adc35fa398a1a194554.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cdf668a86f7a9b8c76f6bf3527e3f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "328d6510fe5bb24a1160",
+    "url": "/static/css/2.07114bf5.chunk.css"
+  },
+  {
+    "revision": "4b16cfa2416a8dcb5817",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "328d6510fe5bb24a1160",
+    "url": "/static/js/2.c7b51dee.chunk.js"
+  },
+  {
+    "revision": "8829a72ac9b90be243333220be0ac6e2",
+    "url": "/static/js/2.c7b51dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b16cfa2416a8dcb5817",
+    "url": "/static/js/main.e546e74c.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  },
+  {
+    "revision": "b590e99ca1d53d99a21c1cd01e987583",
+    "url": "/static/media/calendar-logo.b590e99c.png"
+  },
+  {
+    "revision": "4273dbc532a5b95b3e0edf34f8bc9ff9",
+    "url": "/static/media/edit-logo.4273dbc5.png"
+  },
+  {
+    "revision": "06446287f2d5ccfbf444259727f7a10c",
+    "url": "/static/media/logout-logo.06446287.png"
+  },
+  {
+    "revision": "736ee04a13641af48eb09f5eb3ea31e9",
+    "url": "/static/media/subject-logo.736ee04a.png"
+  },
+  {
+    "revision": "5b74cde8826586f94a4c419fbe106294",
+    "url": "/static/media/task-logo.5b74cde8.png"
+  },
+  {
+    "revision": "4a992c7c922a3b24825180342d55239c",
+    "url": "/static/media/test-logo.4a992c7c.png"
+  },
+  {
+    "revision": "2ea58cae5c4b2883a49f2b671ff98975",
+    "url": "/static/media/trash-logo.2ea58cae.png"
+  },
+  {
+    "revision": "29479ba0435741580ca9f4a467be6207",
+    "url": "/static/media/user-logo.29479ba0.png"
+  }
+]);
